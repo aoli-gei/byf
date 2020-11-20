74ee4b62e99eb0bfd4767c06372759ff9ee315a8
robinxlh <1156689920@qq.com>
2020-11-20 16:00:03 +0800

    .

--- byf/cloudfunctions-aliyun/delrelation/index.js
+++ byf/cloudfunctions-aliyun/delrelation/index.js
@@ -7,13 +7,13 @@ exports.main = async (event, context) => {
 	const res = await collection.where({
 	    _id:event._id 
 	}).update({
-	    lover_id: event.lover_id,
+	    lover_id: '',
 		create_time:Date.now()
 	})
 	const res1 = await collection.where({
 	    _id:event.lover_id
 	}).update({
-	    lover_id: event._id,
+	    lover_id: '',
 		create_time:Date.now()
 	})
 	//返回数据给客户端
