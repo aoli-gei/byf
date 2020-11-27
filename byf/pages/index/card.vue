b91ccf31babaab265c74851280ca514e61579747
robinxlh <1156689920@qq.com>
2020-11-27 23:49:30 +0800

    .

--- byf/pages/index/card.vue
+++ byf/pages/index/card.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<view class="card" v-for="(item,index) in cardList" :key="index">
+		<!-- <view class="card" v-for="(item,index) in cardList" :key="index">
 			<view class="card-item">
 				<view class="item-left">
 					<view class="left-title">{{item.title}}</view>
@@ -26,6 +26,37 @@
 					<image mode="aspectFill" src="/static/juan.png"></image>
 				</view>
 			</view>
+		</view> -->
+		<view class="card" v-for="(item,index) in cardList" :key="index">
+			<view class="card-item">
+				<view class="item-left" v-if="!item.isBack">
+					<view class="left-title">{{item.title}}</view>
+				</view>
+				<view class="item-center"  v-if="!item.isBack">
+					<view class="center-img">
+						<image mode="aspectFill" :src="item.src"></image>
+					</view>
+					<view class="center-title">{{item.tips}}券日期:<text style="margin-left: 10rpx;">{{item.time}}</text></view>
+				</view>
+				<view class="item-right"  v-if="!item.isBack">
+					<view class="right-top">{{item.tip}}</view>
+					<!-- <view class="item-footer selects">使用</view> -->
+					<view class="item-footer" :class="item.isTrue?'select':'selects'" @click="using(item.isTrue,item._id,item.from_id)">{{item.isTrue?"已使用":"使用"}}</view>
+				</view>
+				<view class="item-posone">
+					<image mode="aspectFill" src="/static/love.png"></image>
+					<view class="posone-title">{{item.tips}}</view>
+				</view>
+				<view class="item-postwo"  v-if="!item.isBack">
+					<image mode="aspectFill" src="/static/juan.png"></image>
+				</view>
+				<view class="item-icon" @click="clickBack(index)">
+					<image mode="aspectFill" src="/static/mb-rotate.svg"></image>
+				</view>
+				<view class="item-content"  v-if="item.isBack">
+					{{item.content}}
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -39,69 +70,191 @@
 		},
 		onLoad(option) {
 			if(option.data=="isTrue"){
-				this.cardList=[
-					{
-						title:"举高搞高券",
-						time:"2020.10.01-2020.11.20",
-						tip:"今天你说啥都h1111111",
-						isTrue:false,
-						tips:"LOVE"
-					},{
-						title:"公举抱券",
-						time:"2020.10.04-2020.11.10",
-						tip:"今天你是老大",
-						isTrue:true,
-						tips:"LIKE"
-					},{
-						title:"公举抱券",
-						time:"2020.10.04-2020.11.10",
-						tip:"今天你是说了算",
-						isTrue:false,
-						tips:"LIKE"
-					},{
-						title:"公举抱券",
-						time:"2020.10.04-2020.11.10",
-						tip:"想你了",
-						isTrue:false,
-						tips:"LIKE"
+				// this.cardList=[
+				// 	{
+				// 		title:"举高搞高券",
+				// 		time:"2020.10.01-2020.11.20",
+				// 		tip:"今天你说啥都h1111111",
+				// 		isTrue:false,
+				// 		tips:"LOVE"
+				// 	},{
+				// 		title:"公举抱券",
+				// 		time:"2020.10.04-2020.11.10",
+				// 		tip:"今天你是老大",
+				// 		isTrue:true,
+				// 		tips:"LIKE"
+				// 	},{
+				// 		title:"公举抱券",
+				// 		time:"2020.10.04-2020.11.10",
+				// 		tip:"今天你是说了算",
+				// 		isTrue:false,
+				// 		tips:"LIKE"
+				// 	},{
+				// 		title:"公举抱券",
+				// 		time:"2020.10.04-2020.11.10",
+				// 		tip:"想你了",
+				// 		isTrue:false,
+				// 		tips:"LIKE"
+				// 	}
+				// ]
+				var cardList=[]
+				uniCloud.callFunction({
+					name:'gett1',
+					data:{
+						_id:uni.getStorageSync('lover_id')
+					},
+					success: (res) => {
+						console.log("查询券结果：",res)
+						// this.cardList=res.result.data
+						console.log("查询券：",res.result.data.length)
+						var i
+						
+						for(i in res.result.data){
+							console.log("当前元素：",res.result.data[i])
+							if(res.result.data[i].from_id==uni.getStorageSync('_id')){
+								res.result.data[i].tips="送出"
+							}
+							else{
+								res.result.data[i].tips="收到"
+							}
+							res.result.data[i].time=this.happenTimeFun(res.result.data[0].create_time)
+							res.result.data[i].isBack=false
+							cardList.push(res.result.data[i])
+						}
+						console.log("查询券结果2：",cardList)
 					}
-				]
+				})
+				this.cardList=cardList
 			}else{
-				this.cardList=[
-					{
-						title:"举高高券",
-						time:"2020.10.01-2020.11.20",
-						tip:"今天你说啥都对2131",
-						isTrue:true,
-						tips:"LOVE"
-					},{
-						title:"公举抱券",
-						time:"2020.10.04-2020.11.10",
-						tip:"今天你是老大",
-						isTrue:true,
-						tips:"LIKE"
-					},{
-						title:"公举抱券",
-						time:"2020.10.04-2020.11.10",
-						tip:"今天你是说了算",
-						isTrue:false,
-						tips:"LIKE"
-					},{
-						title:"公举抱券",
-						time:"2020.10.04-2020.11.10",
-						tip:"想你了",
-						isTrue:true,
-						tips:"LIKE"
+				// this.cardList=[
+				// 	{
+				// 		title:"举高高券",
+				// 		time:"2020.10.01-2020.11.20",
+				// 		tip:"今天你说啥都对2131",
+				// 		isTrue:true,
+				// 		tips:"LOVE"
+				// 	},{
+				// 		title:"公举抱券",
+				// 		time:"2020.10.04-2020.11.10",
+				// 		tip:"今天你是老大",
+				// 		isTrue:true,
+				// 		tips:"LIKE"
+				// 	},{
+				// 		title:"公举抱券",
+				// 		time:"2020.10.04-2020.11.10",
+				// 		tip:"今天你是说了算",
+				// 		isTrue:false,
+				// 		tips:"LIKE"
+				// 	},{
+				// 		title:"公举抱券",
+				// 		time:"2020.10.04-2020.11.10",
+				// 		tip:"想你了",
+				// 		isTrue:true,
+				// 		tips:"LIKE"
+				// 	}
+				// ]
+				var cardList=[]
+				uniCloud.callFunction({
+					name:'gett1',
+					data:{
+						_id:uni.getStorageSync('_id')
+					},
+					success: (res) => {
+						console.log("查询券结果：",res)
+						// this.cardList=res.result.data
+						console.log("查询券：",res.result.data.length)
+						var i
+						
+						for(i in res.result.data){
+							console.log("当前元素：",res.result.data[i])
+							if(res.result.data[i].from_id==uni.getStorageSync('_id')){
+								res.result.data[i].tips="送出"
+							}
+							else{
+								res.result.data[i].tips="收到"
+							}
+							res.result.data[i].time=this.happenTimeFun(res.result.data[0].create_time)
+							res.result.data[i].isBack=false
+							cardList.push(res.result.data[i])
+						}
+						console.log("查询券结果2：",cardList)
 					}
-				]
+				})
+				this.cardList=cardList
+			}
+		},
+		methods:{
+			happenTimeFun(num){//时间戳数据处理
+				 //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+				 // let date=Date.now()
+				 let date = new Date(num)
+				 console.log('1233: ',date)
+				 console.log('123: ',Date.now())
+				 let y = date.getFullYear();
+				 let MM = date.getMonth() + 1;
+				 MM = MM < 10 ? ('0' + MM) : MM;//月补0
+				 let d = date.getDate();
+				 d = d < 10 ? ('0' + d) : d;//天补0
+				 let h = date.getHours();
+				 h = h < 10 ? ('0' + h) : h;//小时补0
+				 let m = date.getMinutes();
+				 m = m < 10 ? ('0' + m) : m;//分钟补0
+				 let s = date.getSeconds();
+				 s = s < 10 ? ('0' + s) : s;//秒补0
+				 console.log('现在时间为：',y + '-' + MM + '-' + d)
+				 return y + '-' + MM + '-' + d; //年月日
+				 //return y + '-' + MM + '-' + d + ' ' + h + ':' + m+ ':' + s; //年月日时分秒
+			 },
+			clickBack(index){
+				this.cardList[index].isBack=!this.cardList[index].isBack
+			},
+			using(y,id,fid){
+				if(y){
+					uni.showToast({
+						title:"券已经用过啦！",
+						icon:"none"
+					});
+				}
+				else if(fid==uni.getStorageSync('_id')){
+					uni.showToast({
+						title:"不能用自己送的券哦！",
+						icon:"none"
+					});
+				}
+				else{
+					uni.showModal({
+						title:"使用",
+						content:"确定使用吗?再想想吧",
+						success:(res)=>{
+							console.log(res)
+							uniCloud.callFunction({
+								name:'updt',
+								data:{
+									_id:id
+								},
+								success(res) {
+									setTimeout(function(){
+										// uni.redirectTo({
+									 //    url: '/pages/index/about'
+										// });
+										uni.redirectTo({
+										    url: '/pages/index/card?data=isTrue'
+										});
+									},500)
+								}
+							})
+						}
+					});
+					
+				}
 			}
 		}
 	}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 	page{
-		background-color: #EEE5E0;
+		background-color: #f0e5e0;
 	}
 	.card{
 		width: 100vw;
@@ -109,7 +262,6 @@
 		justify-content: center;
 		align-items: center;
 		.card-item{
-			margin-top: 20rpx;
 			width: 750rpx;
 			height: 450rpx;
 			background-image:url('/static/Copy.png');
@@ -119,29 +271,20 @@
 			display: flex;
 			align-items: center;
 			.item-left{
-				margin-left: 40rpx;
+				margin-left: 53rpx;
 				.left-title{
-					margin-top: 70rpx;
-					margin-bottom: 44rpx;
 					font-size: 36rpx;
 					color: #101010;
 					font-family: PingFangSC-regular;
 					width: 36rpx;
+					height: 300rpx;
 					line-height: 56rpx;
-					margin-left: 16rpx;
-				}
-				.left-icon{
-					width: 60rpx;
-					height: 60rpx;
-					image{
-						width: 100%;
-						height: 100%;
-					}
+					margin-right: 16rpx;
 				}
 			}
 			.item-center{
 				margin-left: 20rpx;
-				margin-right: 62rpx;
+				margin-right: 52rpx;
 				.center-img{
 					width: 380rpx;
 					height: 286rpx;
@@ -172,7 +315,6 @@
 					padding: 20rpx;
 					color: #888888;
 					font-family: Microsoft Yahei;
-					border: 2rpx solid #C0C0C0;
 					border-radius: 10rpx;
 					margin-bottom: 20rpx;
 				}
@@ -197,7 +339,7 @@
 			}
 			.item-posone{
 				position: absolute;
-				top: 0rpx;
+				top: 17rpx;
 				right: 150rpx;
 				width: 140rpx;
 				height: 152rpx;
@@ -213,6 +355,8 @@
 					font-size: 24rpx;
 					color: #EA9518;
 					font-family: SFUIText-regular;
+					transform: rotate(-18deg);
+					letter-spacing: 4rpx;
 				}
 			}
 			.item-postwo{
@@ -224,6 +368,57 @@
 				image{
 					width: 112rpx;
 					height: 124rpx;
+				}
+			}
+			.item-icon{
+				position: absolute;
+				bottom: 64rpx;
+				left: 44rpx;
+				width: 50rpx;
+				height: 50rpx;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.item-content{
+				box-sizing:border-box;
+				padding: 20rpx;
+				width: 380rpx;
+				height: 286rpx;
+				position: absolute;
+				top: 62rpx;
+				left: 127rpx;
+				font-size: 32rpx;
+				line-height: 45rpx;
+				text-indent: 2em;
+				word-break: break-all;
+				text-overflow: ellipsis;
+				display: -webkit-box; 
+				-webkit-box-orient: vertical; 
+				-webkit-line-clamp: 6; 
+				overflow: hidden; 
+			}
+			.item-add{
+				margin-left: 50rpx;
+				margin-right: 30rpx;
+				display: flex;
+				align-items: center;
+				flex-direction: column;
+				width: 486rpx;
+				.add-img{
+					width: 150rpx;
+					height: 150rpx;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+				.add-title{
+					font-size: 32rpx;
+					color: #EA9518;
+					font-family: 方正准圆-undefined;
+					margin-top: 40rpx;
 				}
 			}
 		}
