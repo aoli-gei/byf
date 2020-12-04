3f6609a38e0f871f8f697ef7b912cb3e66d1380d
fengjian <531891437@qq.com>
2020-12-05 00:25:22 +0800

    pages

---
+++ byf/pages/index/bind.vue
@@ -0,0 +1,157 @@
+<template>
+	<view>
+		<view class="content">
+			<view class="header">
+				<view class="header-tip">
+					<view class="tip-title">输入邀请码:</view>
+					<input placeholder="请输入邀请码" />
+				</view>
+				<view class="header-true">
+					<view class="true-title">确定</view>
+				</view>
+			</view>
+			<view class="add-tip">+</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			link(){
+				try {
+				    const value = uni.getStorageSync('_id');
+				    if (value) {
+				        console.log(value);
+				    };
+					uniCloud.callFunction({
+						name:'getu',
+						data:{
+							_id:this.inviteCode
+						}
+					}).then((res)=>{
+						console.log(res)
+						this.number=res.result.affectedDocs
+						console.log('人数：',this.number,res.result.data["0"].lover_id)
+						if(this.number>0&&value!=this.inviteCode&&res.result.data['0'].lover_id==''){
+							// uniCloud.callFunction({
+							// 	name:'updateu',
+							// 	data:{
+							// 		_id:value,
+							// 		lover_id:this.inviteCode
+							// 	}
+							// })
+							uniCloud.callFunction({
+								name:'updateu',
+								data:{
+									_id:this.inviteCode,
+									lover_id:value
+								}
+							}).then((res)=>{
+								uni.hideLoading()
+								uni.showModal({
+									content: "绑定成功！",
+									showCancel: false,
+									success:(res)=>{
+										uni.reLaunch({
+										    url: '/pages/index/index'
+										});
+									}
+								})
+							})
+						}
+						else if(this.number==0||value==this.inviteCode){
+							uni.showModal({
+								content: "绑定失败，该邀请码无效",
+								showCancel: false
+							})
+						}
+						else {
+							uni.showModal({
+								content: "绑定失败，此人已绑定了关系",
+								showCancel: false
+							})
+						}
+					}).catch((err)=>{
+						uni.hideLoading()
+						uni.showModal({
+							content: `绑定错误`,
+							showCancel: false
+						})
+						console.error(err)
+					})
+				} catch (e) {
+				    // error
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content{
+		height: 100vh;
+		width: 100vw;
+		background-image:url('http://47.114.75.140/doc/message/image/18b8b543-9ad7-11e8-aebe-1368d4ec24eb/1605771209571.jpg');
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
+		.header{
+			padding-top: 144rpx;
+			.header-tip{
+				padding: 0 30rpx;
+				height: 88rpx;
+				background-color: #FFFFFF;
+				font-size: 34rpx;
+				color: #000000;
+				display: flex;
+				align-items: center;
+				input{
+					flex: 1;
+					height: 100%;
+					line-height: 88rpx;
+					margin-left: 20rpx;
+				}
+			}
+			.header-true{
+				width: 100vw;
+				margin-top: 120rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				.true-title{
+					width: 400rpx;
+					height: 122rpx;
+					background-color: #fbe6ad;
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					color: #333333;
+					font-size: 32rpx;
+					border-radius: 4rpx;
+					border: 2rpx solid #BBBBBB;
+					border-radius: 30rpx;
+				}
+			}
+		}
+		.add-tip{
+			width: 110rpx;
+			height: 110rpx;
+			background-color: #007AFF;
+			border-radius: 50%;
+			color: #FFFFFF;
+			font-size: 40rpx;
+			overflow: hidden;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			position: fixed;
+			right: 28rpx;
+			bottom: 136rpx;
+		}
+	}
+</style>
---
+++ byf/pages/index/site.vue
@@ -0,0 +1,172 @@
+<template>
+	<view>
+		<view class="content">
+			
+			<!-- 头部 -->
+			<view class="header">
+				<view class="header-title">啊界面危险</view>
+				<view class="header-tip">
+					<navigator class="tip-title" url="/pages/index/index" open-type="switchTab">开溜</navigator>
+				</view>
+			</view>
+			<!-- 尾部 -->
+			<view class="footer">
+				<view class="footer-title">
+					<view class="title-left">星星最终还是要归还给宇宙的啊</view>
+					<view class="title-right">对不起，我想把借的未来还给你</view>
+				</view>
+				<view class="footer-tip">
+					<view class="tip-title" @click="isRelationship">解除关系</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			};
+		},
+		methods:{
+			isRelationship(){
+				uni.showModal({
+					title:"解除关系",
+					content:"确定解除关系吗?再想想吧",
+					success:(res)=>{
+						console.log(res)
+						try {
+						    const value = uni.getStorageSync('_id');
+						    if (value) {
+						        console.log(value);
+						    }
+							uniCloud.callFunction({
+								name:'getu',
+								data:{
+									_id:value
+								}
+							}).then((res)=>{
+								uniCloud.callFunction({
+									name:'delrelation',
+									data:{
+										_id:value,
+										lover_id:res.result.data["0"].lover_id
+									},
+									success(res){
+										uniCloud.callFunction({
+											name:'delt',
+											data:{
+												_id:value
+											}
+										})
+										uni.showModal({
+											content: "解除绑定成功！",
+											showCancel: false,
+											success:(res)=>{
+												console.log(res)
+												if(res.confirm==true){
+													uni.reLaunch({
+													    url: '/pages/index/index'
+													});
+												}
+											}
+										})
+										
+									}
+								})
+							})
+						} catch (e) {
+						    // error
+							uni.showModal({
+								content: "解除绑定失败！",
+								showCancel: false
+							})
+						}
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" >
+	page{
+		background-image:url('http://47.114.75.140/doc/message/image/18b8b543-9ad7-11e8-aebe-1368d4ec24eb/1605771188509.jpg');
+		background-repeat: no-repeat;
+		background-size:100% 100%;
+	}
+	.content{
+		height: 100vh;
+		width: 100vw;
+		.header{
+			width: 100vw;
+			height: 40vh;
+			border-bottom: 8rpx solid #FFAC00;
+			.header-title{
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-size: 72rpx;
+				color: #666666;
+				font-family: PingFangSC-regular;
+				padding-top: 100rpx;
+			}
+			.header-tip{
+				width: 100vw;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				margin-top: 100rpx;
+				.tip-title{
+					width: 160rpx;
+					height: 68rpx;
+					color: #101010;
+					text-align: center;
+					line-height: 68rpx;
+					background-color: #FFFFFF;
+					border: 2rpx solid #BBBBBB;
+					border-radius: 4rpx;
+					font-size: 30rpx;
+				}
+			}
+		}
+		.footer{
+			width: 100vw;
+			height: 59vh;
+			.footer-title{
+				padding: 330rpx 30rpx 0 30rpx;
+				font-family: PingFangSC-regular;
+				font-size: 36rpx;
+				color: #333333;
+				.title-left{
+					line-height: 70rpx;
+					text-align: left;
+				}
+				.title-right{
+					line-height: 70rpx;
+					text-align: right;
+				}
+			}
+			.footer-tip{
+				width: 100vw;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				margin-top: 44rpx;
+				.tip-title{
+					width: 204rpx;
+					height: 68rpx;
+					color: #101010;
+					text-align: center;
+					line-height: 68rpx;
+					background-color: #FFFFFF;
+					border: 2rpx solid #BBBBBB;
+					border-radius: 4rpx;
+					font-size: 30rpx;
+				}
+			}
+		}
+	}
+</style>
